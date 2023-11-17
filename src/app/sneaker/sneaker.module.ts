432d7ee58a0ea6be3a72321e84d153cac9be319e
Wanderson Ramos <wandersonramos740@gmail.com>
2023-11-17 20:53:06 -0300

    feat: create module sneaker

---
+++ src/app/sneaker/sneaker.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SneakerRoutingModule } from './sneaker-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SneakerRoutingModule
+  ]
+})
+export class SneakerModule { }
