f67ad856c98d814d96b93ed18065af6b168fee2a
Wanderson Ramos <wandersonramos740@gmail.com>
2023-11-17 21:00:19 -0300

    feat: create component home-page

--- src/app/sneaker/sneaker.module.ts
+++ src/app/sneaker/sneaker.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SneakerRoutingModule } from './sneaker-routing.module';
+import { HomePageComponent } from './containers/home-page/home-page.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     SneakerRoutingModule
