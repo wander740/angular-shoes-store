0d15d4d842a59ba74429a545ce70ad210f8e262b
Wanderson Ramos <wandersonramos740@gmail.com>
2023-11-17 21:05:07 -0300

    feat: add main title

--- src/app/sneaker/containers/home-page/home-page.component.ts
+++ src/app/sneaker/containers/home-page/home-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css', 'home-page.responsive.component.css']
 })
 export class HomePageComponent {
 
